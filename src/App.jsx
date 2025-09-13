266be2fe426a57843de158da35b1169e17566cfb
jaishreeshyam7 <163719816+jaishreeshyam7@users.noreply.github.com>
2025-09-14 00:35:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
-// import './App.css';
+ import React from 'react';
+ import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+ import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <SortingVisualizer />
-//     </div>
-//   );
-// }
+ function App() {
+   return (
+     <div className="App">
+       <SortingVisualizer />
+     </div>
+   );
+ }
 
-// export default App;
-import React from 'react';
+ export default App;
+//import React from 'react';
 
-function App() {
-  return <div>Hello</div>;
-}
+//function App() {
+  //return <div>Hello</div>;
+//}
 
-export default App;
+//export default App;
